f8e4b0de73bb46a0ee5b463080e0260d39627ebc
Tjornfelt <madstjornfelt@gmail.com>
2015-04-14 11:03:58 +0200

    added frontpage and textpage

---
+++ RhinoTech/RhinoTech.App/Controllers/FrontPageController.cs
@@ -0,0 +1,29 @@
+﻿using RhinoTech.App.Classes.Cms;
+using RhinoTech.App.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web;
+using Umbraco.Web.Models;
+
+namespace RhinoTech.App.Controllers
+{
+    public class FrontPageController : MasterController
+    {
+        //
+        // GET: /FrontPage/
+
+        public ActionResult FrontPage(RenderModel renderModel)
+        {
+            FrontPageModel model = new FrontPageModel()
+            {
+                Header = CurrentPage.GetPropertyValue(DocTypes.Frontpage.Header),
+                BodyText = CurrentPage.GetPropertyValue(DocTypes.Frontpage.BodyText),
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ RhinoTech/RhinoTech.App/Controllers/MasterController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Core;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+using Umbraco.Web.Mvc;
+
+
+namespace RhinoTech.App.Controllers
+{
+    public class MasterController : RenderMvcController
+    {
+        protected override ViewResult View(string viewName, string masterName, object model)
+        {
+            IPublishedContent frontPage = CurrentPage.AncestorOrSelf(1);
+
+            return base.View(viewName, masterName, model);
+        }
+    }
+}
