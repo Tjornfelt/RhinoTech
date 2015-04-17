853e7ce3576e5648258adb3fbda645fb5d1c971f
Tjornfelt <madstjornfelt@gmail.com>
2015-04-17 10:37:01 +0200

    news items, mobile menu and script section

---
+++ RhinoTech/RhinoTech.App/Models/ViewModels/NewsItemModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RhinoTech.App.Models.ViewModels
+{
+    public class NewsItemModel
+    {
+        public string Header { get; set; }
+        public DateTime Date { get; set; }
+        public IHtmlString BodyText { get; set; }
+    }
+}
