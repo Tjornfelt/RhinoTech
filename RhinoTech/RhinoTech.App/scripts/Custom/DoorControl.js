5b62436200448b149ee98252f619710d2f2a8397
Backster <Christianandersen@ofir.dk>
2015-04-20 14:21:28 +0200

    Two javascripts, and changes to Warehouse.

---
+++ RhinoTech/RhinoTech.App/scripts/Custom/DoorControl.js
@@ -0,0 +1,41 @@
+﻿function red(query) {
+
+    $.ajax(
+        {
+            url: "http://10.129.54.119/doorControl.py?red=" + query,
+            success: function (data) {
+                alert(data);
+            }
+        });
+}
+function green(query) {
+    
+    $.ajax(
+       {
+           url: "http://10.129.54.119/doorControl.py?green=" + query,
+           success: function (data) {
+               alert(data);
+           }
+       });
+}
+function red2(query) {
+    
+    $.ajax(
+      {
+          url: "http://10.129.54.119/doorControl.py?red2=" + query,
+          success: function (data) {
+              alert(data);
+          }
+      });
+}
+
+function green2(query) {
+
+    $.ajax(
+      {
+          url: "http://10.129.54.119/doorControl.py?green2=" + query,
+          success: function (data) {
+              alert(data);
+          }
+      });
+}
